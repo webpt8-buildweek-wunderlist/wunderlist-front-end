881ff3ebbf19b4e78c492da0f8658659337f644c
Oksana <oxanakril@gmail.com>
2019-09-22 21:36:19 -0500

    header and navbar with basic styling

--- wunderlist/src/App.js
+++ wunderlist/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import SignUp from './components/Sign_up';
 import './App.css';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <Header />
+       <NavBar />
        <h1>Wunderlist</h1>
        <SignUp />
       </header>
--- wunderlist/src/components/NavBar.js
+++ wunderlist/src/components/NavBar.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+import { NavLink } from "react-router-dom";
+import './NavBar.css';
 
-export default function NavBar (){
-    return(
-        <div>
-            
-        </div>
-    )
-}
+
+
+export default class NavBar extends Component {
+    state = { activeItem: 'home' }
+  
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  
+    render() {
+      const { activeItem } = this.state
+  
+      return (
+        <Segment inverted className = "NavBarSegment">
+          <Menu inverted secondary  className = "NavBarMenu">
+
+          {/* <NavLink to="/">
+            <Menu.Item
+              className = "MenuItem"
+              name='home_page'
+              active={activeItem === 'home_page'}
+              onClick={this.handleItemClick}
+            />
+          </NavLink> */}
+
+          <NavLink to="/to_do_list">    
+            <Menu.Item
+              className = "MenuItem"
+              name='to_do_list'
+              active={activeItem === 'to_do_list'}
+              onClick={this.handleItemClick}
+            />
+          </NavLink>
+
+          <NavLink to="/callendar">
+            <Menu.Item
+              className = "MenuItem"
+              name='callendar'
+              active={activeItem === 'callendar'}
+              onClick={this.handleItemClick}
+            />
+          </NavLink>
+
+          <NavLink to="/current_day">
+            <Menu.Item
+              className = "MenuItem"
+              name='current_day'
+              active={activeItem === 'current_day'}
+              onClick={this.handleItemClick}
+            />
+          </NavLink>
+
+          <NavLink to="/to_do_list">
+            <Menu.Item
+              className = "MenuItem"
+              name='log_out'
+              active={activeItem === 'log_out'}
+              onClick={this.handleItemClick}
+            />
+          </NavLink>
+
+          </Menu>
+        </Segment>
+      )
+    }
+  }
+
