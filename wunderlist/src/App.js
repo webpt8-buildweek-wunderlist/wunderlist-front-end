51d3f997560a853b43a45d6e494085e1853cc6f0
Oksana <oxanakril@gmail.com>
2019-09-25 17:47:09 -0500

    login

--- wunderlist/src/App.js
+++ wunderlist/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import SignUp from './components/Sign_up';
 import Login from './components/Login';
 import './App.css';
-import {Route} from "react-router-dom";
-// import {PrivateRoute} from "./components/PrivateRoute";
+import {Route, Switch} from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 // import TheMain from "./components/Main";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
@@ -15,17 +15,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Switch>
        {/* <PrivateRoute path = "/user" component = {TheMain} /> */}
-       <Route path = "/" component = {Header} />
-        <Route path = "/" component = {NavBar} />
-        <Route path = "/" component = {Search} />
-        <Route  path='/calendar' component={Calendar} />
+        <PrivateRoute exact path = "/user" component = {Header} />
+        <PrivateRoute exact path = "/user" component = {NavBar} />
+        <PrivateRoute exact path = "/user" component = {Search} />
+        <Route exact path='/calendar' component={Calendar} />
        
        <h1>Wunderlist</h1>
        {/* <Route exact path='/' component={HomePage} /> */}
-       <Route exact path='/' component={Login} />
+       <Route exact path='/login' component={Login} />
        <Route exact path='/signup' component={SignUp} />
-    
+      </Switch>
       </header>
     </div>
   );
