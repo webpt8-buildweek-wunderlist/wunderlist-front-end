2a808053421058292ca2c876911e463c69f62020
YasirHasn9 <hassanyasir223@gmail.com>
2019-09-26 19:16:50 -0700

    fixing some bugs

--- wunderlist/src/components/tasks/taskList/Task.js
+++ wunderlist/src/components/tasks/taskList/Task.js
@@ -11,54 +11,49 @@ const Span = styled.span`
 `;
 
 const TaskDiv = styled.div`
-width:100%;
-display:flex;
-justify-content: space-around;
-align-items: center;
-height:100px;
-`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100px;
+`;
 const DivOfLi = styled.div`
-width:72%;
-background:#fff;
-display:flex;
-justifiy-contnent:flex-start;
-align-items: center;
-padding:10px;
-border-radius: 20px;
+  width: 72%;
+  background: #fff;
+  display: flex;
+  justifiy-contnent: flex-start;
+  align-items: center;
+  padding: 10px;
+  border-radius: 20px;
 `;
 const Li = styled.li`
-  background:#fff;
-  margin-left:40px;
-  font-size:1.2rem
+  background: #fff;
+  margin-left: 40px;
+  font-size: 1.2rem;
 `;
 
 const DivOfBtn = styled.div`
-width:17%;
-background:#FFF;
-padding:5px;
-border-radius: 20px;
-&:hover{
-  background-color: Lavender;
-}
+  width: 17%;
+  background: #fff;
+  padding: 5px;
+  border-radius: 20px;
+  &:hover {
+    background-color: Lavender;
+  }
 `;
-function Task({ task, isTaskCompleted , deletTask }) {
+function Task({ task, isTaskCompleted, deletTask }) {
+  console.log("this is task", task);
   return (
     <TaskDiv>
-
       <DivOfLi onClick={() => isTaskCompleted(task.id)}>
-      <Li>
+        <Li>
           {task.isCompleted ? <del>{`${task.task}`}</del> : `${task.task}`}
         </Li>
       </DivOfLi>
       <DivOfBtn>
-      <Button outline color="info">
-           <Link to={`to_do_list/${task.id}`}>Edit</Link>
-      </Button>
-      </DivOfBtn>
-      <DivOfBtn>
-      <Button outline color="danger" onClick={() =>deletTask(task.id)}>
-           Delete
-      </Button>
+        <Button outline color="danger" onClick={() => deletTask(task.id)}>
+          Delete
+        </Button>
       </DivOfBtn>
     </TaskDiv>
   );
